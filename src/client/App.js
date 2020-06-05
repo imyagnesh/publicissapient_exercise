d25edea54f39ac325f22913cb9a414e8c4aed533
Yagnesh Modh <yagnesh.modh@gmail.com>
2020-06-06 01:18:47 +0530

    fix package issue

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import loadable from '@loadable/component';
+import loadable from '@loadable/component';
 import ErrorBoundary from './components/ErrorBoundry';
 import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
 import './main.css';
 
 // const HomePage = loadable(() => import(/* webpackChunkName: 'HomePage' */ './pages/HomePage'));
-// const NotFoundPage = loadable(() =>
-//   import(/* webpackChunkName: 'NotFoundPage' */ './pages/NotFoundPage'),
-// );
+const NotFoundPage = loadable(() =>
+  import(/* webpackChunkName: 'NotFoundPage' */ './pages/NotFoundPage'),
+);
 
 const App = () => {
   return (
