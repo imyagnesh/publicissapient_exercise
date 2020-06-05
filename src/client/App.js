8ba9626becc1076bdba6b7a3cbcb2f019f5a32e5
Yagnesh Modh <yagnesh.modh@gmail.com>
2020-06-06 02:01:36 +0530

    remove notfound

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import loadable from '@loadable/component';
+// import loadable from '@loadable/component';
 import ErrorBoundary from './components/ErrorBoundry';
 import HomePage from './pages/HomePage';
 import './main.css';
 
 // const HomePage = loadable(() => import(/* webpackChunkName: 'HomePage' */ './pages/HomePage'));
-const NotFoundPage = loadable(() =>
-  import(/* webpackChunkName: 'NotFoundPage' */ './pages/NotFoundPage'),
-);
+// const NotFoundPage = loadable(() =>
+//   import(/* webpackChunkName: 'NotFoundPage' */ './pages/NotFoundPage'),
+// );
 
 const App = () => {
   return (
@@ -17,7 +17,6 @@ const App = () => {
       <ErrorBoundary>
         <Switch>
           <Route path="/:page?" exact component={HomePage} />
-          <Route component={NotFoundPage} />
         </Switch>
       </ErrorBoundary>
     </main>
